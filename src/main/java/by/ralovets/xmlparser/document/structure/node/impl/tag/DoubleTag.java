74e5029b2db5bd321653b19262bd308310aae1f7
OlgaSmolyakova <ollaniel@gmail.com>
2021-02-16 12:26:44 +0300

    Update DoubleTag.java

--- src/main/java/by/ralovets/xmlparser/document/structure/node/impl/tag/DoubleTag.java
+++ src/main/java/by/ralovets/xmlparser/document/structure/node/impl/tag/DoubleTag.java
@@ -1,6 +1,6 @@
 package by.ralovets.xmlparser.document.structure.node.impl.tag;
 
-public class DoubleTag extends Tag {
+public class DoubleTag extends Tag {// мы учили, что такое пустое наследование бесполезно и является ошибкой проектирования. Вводить класс только из-за имени - ошибка, это же не исключение
 
     public DoubleTag() {
         super(false);
